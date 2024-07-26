e75191347e33b062324da394d491bc9a9051bddc
Phil5184 <p3mai@ucsd.edu>
2024-07-26 13:14:55 -0700

    testing push and pull

--- githubTest.java
+++ githubTest.java
@@ -5,6 +5,6 @@ this is my 2nd edit /phillip 2024
 this is my 3rd edit
 */
 
-"testing to see if the git push merges smartly without messing crap up"
+//"testing to see if the git push merges smartly without messing crap up"
 
-"testing to see if git pull and push will do something smart"
+//"testing to see if git pull and push will do something smart"
