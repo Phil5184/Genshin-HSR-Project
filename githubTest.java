c604b16251c67cef9c3632b2196ecc03968486c6
Phil5184 <p3mai@ucsd.edu>
2024-07-26 13:09:53 -0700

    testing push

--- githubTest.java
+++ githubTest.java
@@ -1,7 +1,8 @@
 /*
 hi this is phillip
 this is my edit /phillip 2024
-Merging test to hopefully prevent divergence /hugh 2024
 this is my 2nd edit /phillip 2024
 this is my 3rd edit
 */
+
+"testing to see if the git push merges smartly without messing crap up"
